4c785d133da829a22c63193cd5ce32984ad4df66
Markus Erkkilä <n0erma00@students.oamk.fi>
2021-11-09 13:32:02 +0200

    Route fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import { Switch, Route } from 'react-router';
 import './App.css';
-<<<<<<< HEAD
 import Home from './components/Home';
 
-function App() {
-  return (
-    <>
-    
-      <Switch>
-        <Route path="/" component={Home} exact />
-      </Switch>
-    
-    </>
-=======
 import Navbar from './components/Navbar';
 import Content from './components/Content';
 
@@ -21,8 +10,10 @@ function App() {
     <div>
       <Navbar />
       <Content />
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
     </div>
->>>>>>> 8e4cbe0571471355729c14b31c418536c40c3b6e
   );
 }
 
--- src/components/Frontpage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Frontpage() {
-  return (
-    <div>
-      <p>testi</p>
-    </div>
-  );
-}
