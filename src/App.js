f16edb2d47b4e53545784151f019ddf626ec5449
Yunis Sule <yunis.sule@gmail.com>
2021-11-09 14:05:05 +0200

    Merge fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-import { Switch, Route } from 'react-router';
 import './App.css';
-<<<<<<< HEAD
-import Home from './components/Home';
-
-function App() {
-  return (
-    <>
-    
-      <Switch>
-        <Route path="/" component={Home} exact />
-      </Switch>
-    
-    </>
-=======
 import Navbar from './components/Navbar';
 import Content from './components/Content';
 import Infobanner from './components/Infobanner';
@@ -24,7 +10,6 @@ function App() {
       <Navbar />
       <Content />
     </div>
->>>>>>> 8e4cbe0571471355729c14b31c418536c40c3b6e
   );
 }
 
--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -3,7 +3,16 @@ import React from 'react';
 export default function Frontpage() {
   return (
     <div>
-      <p>testi</p>
+      <>
+        <div class="d-flex flex-column bd-highlight mb-3" id="banner">
+          <h1 className="text-center" id="header">
+            Frisbeekauppa
+          </h1>
+          <p className="text-center" id="text">
+            Kiekkojen erikiosliike<br></br>Parhaat tarjoukset, isoin valikoima, nopea toimitus
+          </p>
+        </div>
+      </>
     </div>
   );
 }
