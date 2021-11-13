21de332e4ca4d606ad10159866172eedc3006164
Yunis Sule <yunis.sule@gmail.com>
2021-11-13 15:12:12 +0200

    Modified search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 import Logo from '../images/Fribashoplogo.svg';
 import React from 'react';
-import { Navbar, NavbarToggler, NavbarBrand, Collapse, NavItem, Nav, NavLink, DropdownItem, DropdownMenu, UncontrolledDropdown, Input, DropdownToggle } from 'reactstrap';
+import { Navbar, NavbarToggler, NavbarBrand, Collapse, NavItem, Nav, NavLink, Input, Button } from 'reactstrap';
 import { useState } from 'react';
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
+  const [query, setQuery] = useState('');
+
+  function handleSubmit(e) {
+    console.log(query);
+  }
 
   return (
     <div>
@@ -44,19 +50,28 @@ export default function NavBar() {
           </Nav>
         </Collapse>
         <div id="navItems">
-          <UncontrolledDropdown inNavbar>
-            <DropdownToggle caret nav>
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16" color="white">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-              </svg>
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>
-                <Input />
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-
+          <NavLink className="searchBox">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+              color="white"
+              onClick={function noRefCheck() {
+                setSearchOpen(!searchOpen);
+              }}
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+            <Collapse isOpen={searchOpen} horizontal>
+              <form className="searchBox" onSubmit={handleSubmit}>
+                <Input value={query} onChange={(e) => setQuery(e.target.value)} />
+                <Button>hae</Button>
+              </form>
+            </Collapse>
+          </NavLink>
           <NavLink>
             <Link to="ostoskori">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-basket" viewBox="0 0 16 16" color="white">
