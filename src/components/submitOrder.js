6ff62f78f16a7bb5a00bb67a10b3a3fb0bbbf179
Yunis Sule <yunis.sule@gmail.com>
2021-12-07 17:24:26 +0200

    Added form handling to submitOrder.js

--- src/components/submitOrder.js
+++ src/components/submitOrder.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import { Button, Table, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
 import axiosInstance from '../axios';
 
 export default function SubmitOrder({ modal, close, cart }) {
+  const [formdata, setFormdata] = useState([]);
+
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormdata((values) => ({ ...values, [name]: value }));
+  }
+
   function closeModal() {
     close();
   }
 
   function submit() {
+    let json = JSON.stringify({
+      cart: cart,
+      form: formdata
+    });
+
     axiosInstance
-      .post('/order/postorder.php?user_id=1', cart)
+      .post('/order/postorder.php?user_id=1', json)
       .then((response) => {
         alert('Tilaus lähetetty');
       })
@@ -54,10 +68,10 @@ export default function SubmitOrder({ modal, close, cart }) {
             <Label>Laskutustapa</Label>
             <FormGroup tag="fieldset">
               <FormGroup check>
-                <Input name="radio1" type="radio" /> <Label check>Sähköposti</Label>
+                <Input name="radio1" type="radio" value="email" onChange={handleChange} /> <Label check>Sähköposti</Label>
               </FormGroup>
               <FormGroup check>
-                <Input name="radio1" type="radio" /> <Label check>Paperilasku</Label>
+                <Input name="radio1" type="radio" value="letter" onChange={handleChange} /> <Label check>Paperilasku</Label>
               </FormGroup>
             </FormGroup>
           </Form>
